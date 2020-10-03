ed09a7a2e5915fcab3b078267c922eb36710d957
Sunil <sunilsv26@gmail.com>
2020-10-03 16:24:40 +0530

    SalesByMatch Second Commit Wrote Javascript Function

--- InterviewPreparationKit/WarmUpChallanges/SalesByMatches/app.js
+++ InterviewPreparationKit/WarmUpChallanges/SalesByMatches/app.js
@@ -1,1 +1,15 @@
-console.log('Welcome')
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    ar.sort();
+    let count = 0;
+    for (let i=0;i<n;i++){
+        if(ar[i]==ar[i+1]){
+            i++;
+            count++;
+        }
+    }
+    return count;
+
+}
+
+console.log( sockMerchant(9,[10,20,20,10,10,30,50,10,20]));
